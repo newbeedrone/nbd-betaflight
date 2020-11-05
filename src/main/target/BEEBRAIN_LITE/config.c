23253543cc0e9375b12bc83f5982133195d5a686
Gordon Mak <mgd@sequoiatech.co>
2020-11-05 17:01:12 +0800

    Fixed BeeBrain Lite vtx power label

--- src/main/target/BEEBRAIN_LITE/config.c
+++ src/main/target/BEEBRAIN_LITE/config.c
@@ -53,7 +53,7 @@ void targetConfiguration(void)
 {
 #ifdef USE_VTX_TABLE
     const uint16_t vtxTablePowerValues[VTX_TABLE_MAX_POWER_LEVELS] = {0, 1, 2};
-    const char *vtxTablePowerLabels[VTX_TABLE_MAX_POWER_LEVELS] = {"OFF", "MIN", "MAX"};
+    const char *vtxTablePowerLabels[VTX_TABLE_MAX_POWER_LEVELS] = {"OFF", "10", "15"};
     vtxTableConfigMutable()->powerLevels = 3;
 
     #if defined(USE_VTX_US_TABLE)
