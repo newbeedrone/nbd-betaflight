b6be2d091a1be31958cd9e938f7851aa97a5f87e
Romin Chen <cx@sequoiatech.co>
2022-05-25 16:16:11 +0800

    Fixed Motor issue

--- src/main/target/INFINITY_AIO_V2_PRO_F7/target.c
+++ src/main/target/INFINITY_AIO_V2_PRO_F7/target.c
@@ -29,10 +29,10 @@
 
 const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
 
-    DEF_TIM(TIM3, CH1, PC6, TIM_USE_MOTOR,          0, 0), // M1
-    DEF_TIM(TIM3, CH2, PC7, TIM_USE_MOTOR,          0, 0), // M2
-    DEF_TIM(TIM3, CH3, PC8, TIM_USE_MOTOR,          0, 0), // M3
-    DEF_TIM(TIM3, CH4, PC9, TIM_USE_MOTOR,          0, 0), // M4
+    DEF_TIM(TIM3, CH4, PC9, TIM_USE_MOTOR,          0, 0), // M1
+    DEF_TIM(TIM3, CH3, PC8, TIM_USE_MOTOR,          0, 0), // M2
+    DEF_TIM(TIM3, CH2, PC7, TIM_USE_MOTOR,          0, 0), // M3
+    DEF_TIM(TIM3, CH1, PC6, TIM_USE_MOTOR,          0, 0), // M4
 
     DEF_TIM(TIM1, CH2, PA9, TIM_USE_LED,            0, 0), // LED Strip
     DEF_TIM(TIM4, CH2, PD13,TIM_USE_BEEPER,         0, 0)  // Beeper
