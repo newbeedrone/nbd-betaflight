cbe9f05b9375178e11a10c395fe7848396b5e96e
Romin <cx@sequoiatech.co>
2022-09-07 09:37:41 +0800

    Added target BBBL V4 HP ELRS

---
+++ src/main/target/BEEBRAIN_BL_V4_HP_ELRS/target.mk
@@ -0,0 +1,8 @@
+F411_TARGETS += $(TARGET)
+FEATURES     += VCP ONBOARDFLASH
+
+TARGET_SRC = \
+            drivers/max7456.c \
+            drivers/vtx_rtc6705.c \
+            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_spi_bmi270.c \
