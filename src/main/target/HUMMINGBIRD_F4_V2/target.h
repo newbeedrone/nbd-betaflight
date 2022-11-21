364addba01ed126c58f22ba3ed061ef387d296b6
Romin <cx@sequoiatech.co>
2022-11-21 12:07:38 +0800

    Added beeper for hummingbird f4 v2

--- src/main/target/HUMMINGBIRD_F4_V2/target.h
+++ src/main/target/HUMMINGBIRD_F4_V2/target.h
@@ -141,5 +141,7 @@
 #define TARGET_IO_PORTC                 0xffff
 #define TARGET_IO_PORTD                 (BIT(2))
 
+#define USE_BEEPER
+
 #define USABLE_TIMER_CHANNEL_COUNT      5
 #define USED_TIMERS                     ( TIM_N(2) | TIM_N(3) | TIM_N(4) )
