226f5d24101d86a930d2daaa2d42115d376d5db8
yy <yangye19940404@gmail.com>
2023-03-18 13:21:00 +0800

    feature: add bl v4 crsf 65

---
+++ src/main/target/BEEBRAIN_BL_V4_CRSF_65/target.mk
@@ -0,0 +1,9 @@
+F411_TARGETS += $(TARGET)
+FEATURES     += VCP
+
+TARGET_SRC = \
+            drivers/max7456.c \
+            drivers/vtx_rtc6705.c \
+            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+            drivers/accgyro/accgyro_spi_bmi270.c
+CFLAGS += -DCLOUD_BUILD
