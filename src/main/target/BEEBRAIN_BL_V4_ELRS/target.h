6748a95098dadd4c236f2681f55540b19966fc2b
Gordon Mak <winchester.mygod@hotmail.com>
2023-07-07 15:55:15 +0800

    Fixed target build issues

--- src/main/target/BEEBRAIN_BL_V4_ELRS/target.h
+++ src/main/target/BEEBRAIN_BL_V4_ELRS/target.h
@@ -128,5 +128,3 @@
 
 #define USABLE_TIMER_CHANNEL_COUNT      7
 #define USED_TIMERS                     ( TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) )
-
-#define USE_TARGET_CONFIG
--- src/main/target/INFINITY_F7/target.mk
+++ src/main/target/INFINITY_F7/target.mk
@@ -3,5 +3,5 @@ FEATURES       += VCP ONBOARDFLASH
 
 TARGET_SRC = \
             drivers/max7456.c \
-            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270.c \
+            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
             drivers/accgyro/accgyro_spi_bmi270.c \
