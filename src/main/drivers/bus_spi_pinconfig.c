07c5819d5f8702adb1f8a934be5dd1851466c6fe
LiuYongNan <844550332@qq.com>
2025-04-03 19:10:13 +0800

    add target `LIONBEE_V2`

--- src/main/drivers/bus_spi_pinconfig.c
+++ src/main/drivers/bus_spi_pinconfig.c
@@ -406,6 +406,7 @@ const spiHardware_t spiHardware[] = {
             { DEFIO_TAG_E(PB5),  GPIO_MUX_6},
             { DEFIO_TAG_E(PC12),  GPIO_MUX_6},
             { DEFIO_TAG_E(PD0),  GPIO_MUX_6},
+            { DEFIO_TAG_E(PB0),  GPIO_MUX_7},
         },
         .af= 0x00,
         .rcc = RCC_APB1(SPI3),
